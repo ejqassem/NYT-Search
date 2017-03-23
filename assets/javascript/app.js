5528cb738813c63c1ace03c94dd55defdc6e35df
Eyad Qassem <eyadqassem@Eyads-MacBook-Air.local>
2017-03-22 21:38:51 -0400

    added optional start year criteria in AJAX request

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,7 @@ $("#search").on("click", function(event) {
   var page;
   var term = $("#term").val().trim();
   var records = $("#records").val().trim();
+  var beginDate = $("#start").val().trim();
 
 
   var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=9d7068958bdf4f8db4b43ee759924b10";
@@ -16,7 +17,18 @@ $("#search").on("click", function(event) {
     // defaults to one page if no user input on input criteria
   url =   "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=9d7068958bdf4f8db4b43ee759924b10&q=" + term + "&page=1";
   }
-
+  if($("#term").val && $("#records").val) {
+    if (records < 10) {
+      page = 1;
+    }
+    else if (11 <= records <= 20) {
+      page = 2;
+    }
+    else if (21 <= records <= 30) {
+      page = 3;
+    }
+    url =   "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=9d7068958bdf4f8db4b43ee759924b10&q=" + term +"&page=" + page;
+  }
   else if($("#term").val && $("#records").val && $("#start").val) {
     if (records < 10) {
       page = 1;
@@ -28,7 +40,7 @@ $("#search").on("click", function(event) {
       page = 3;
     }
 
-    url =   "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=9d7068958bdf4f8db4b43ee759924b10&q=" + term +"&page=" + page;
+    url =   "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=9d7068958bdf4f8db4b43ee759924b10&q=" + term +"&page=" + page +"&begin-date=" + beginDate;
 
   }
 
